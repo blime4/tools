0b7dcebc68f338b5eb2f5a43fd6c8ff6b58ecb9c
shaobo.xie <shaobo.xie@denglin.ai>
2023-06-12 17:18:43 +0800

    support hook_specifiy_modules

--- hooktools/tracer.py
+++ hooktools/tracer.py
@@ -137,8 +137,7 @@ class DumpPtFileTracer(TracerBase):
         if self.has_hacker:
             self.hacker.hack(self.hook_forward_fn, self.hook_backward_fn)
 
-        self.hook_specifiy_modules = self.dump_pt_hook_options.get("hook_specifiy_modules", None)
-        print("for debug: self.hook_specifiy_modules : ", self.hook_specifiy_modules)
+        self.hook_specifiy_modules = self.dump_pt_hook_options.get("hook_specifiy_modules", {})
 
     def hook_forward_fn(self, module, input, output):
         self._hook_impl(module, input, output, "Forward")
@@ -176,7 +175,7 @@ class DumpPtFileTracer(TracerBase):
         self.step = step
         for name, param in self.model.named_parameters():
             if param is not None:
-                if self.hook_specifiy_modules is None or any(str(name).startswith(specifiy_module) for specifiy_module in self.hook_specifiy_modules):
+                if self._check_if_need_to_save(name):
                     self._set_current_save_path("Gradient")
                     hook_data = NewHookData(module=name, gradient=param, gradient_grad=param.grad)
                     self._save_pt_data(hook_data, mode="Gradient")
@@ -184,7 +183,7 @@ class DumpPtFileTracer(TracerBase):
 
     def _hook_impl(self, module, input, output, mode="Forward"):
         if self.is_trace:
-            if self.hook_specifiy_modules is None or any(str(module).startswith(specifiy_module) for specifiy_module in self.hook_specifiy_modules):
+            if self._check_if_need_to_save(module):
                 self._set_current_save_path(mode)
                 hook_data = NewHookData(module=module, input=input, output=output)
                 self._save_pt_data(hook_data, mode=mode)
@@ -202,6 +201,18 @@ class DumpPtFileTracer(TracerBase):
         with open(pt_file, "wb+") as f:
             self._save_module_name_file_path_map(data.module_name, pt_file)
             torch.save(data, f)
+
+    def _check_if_need_to_save(self, module_name):
+        if not self.hook_specifiy_modules or len(self.hook_specifiy_modules) == 0:
+            return True
+        if "starts_with" in self.hook_specifiy_modules:
+            if any(str(module_name).startswith(specifiy_module) for specifiy_module in self.hook_specifiy_modules["starts_with"]):
+                return True
+        if "regular_expression" in self.hook_specifiy_modules:
+            import re
+            if any(re.match(rf"{pattern}", str(module_name)) for pattern in self.hook_specifiy_modules["regular_expression"]):
+                return True
+        return False
 
 
 class Tracer(object):
