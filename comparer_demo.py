0c1c757039c52d00b00f93742192e9b4df53d1cd
shaobo.xie <shaobo.xie@denglin.ai>
2023-06-21 16:49:48 +0800

    update for siyi

--- comparer_demo.py
+++ comparer_demo.py
@@ -2,8 +2,8 @@ import os
 from hooktools import Comparer
 import hooktools
 
-config_path = os.path.join(hooktools.__path__[0], "config")
+# config_path = os.path.join(hooktools.__path__[0], "config")
 
 if __name__ == "__main__":
-    compare = Comparer(os.path.join(config_path, "comparer_demo.yaml"))
+    compare = Comparer(("./comparer_demo.yaml"))
     compare.compare()
